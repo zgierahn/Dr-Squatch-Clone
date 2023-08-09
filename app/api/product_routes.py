f86e298a5805f2dc3784d31440da416d4eed87a6
Zach Gierahn <zkg2789@yahoo.com>
2023-08-09 16:34:39 -0700

    added-product-routes

---
+++ app/api/product_routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+from app.models import Product
+
+
+product_routes = Blueprint('products', __name__)
+
+
+#Get Products
+@product_routes.route('/')
+def products():
+    products = Product.query.all()
+    print('products', products)
+    return [product.to_dict() for product in products]
+
+
+#Get Product by Id
+@product_routes.route('/<int:id>')
+def single_product(id):
+    product = Product.query.get(id)
+    # print('show me the product',  product.to_dict())
+    return product.to_dict()
