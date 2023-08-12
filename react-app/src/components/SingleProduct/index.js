61ab0ae599531d574604f1857792c31528f741e6
Zach Gierahn <zkg2789@yahoo.com>
2023-08-12 16:03:52 -0700

    product-wireframing

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -4,8 +4,37 @@ function SingleProduct() {
 
 
   return (
-      <div>SingleProduct</div>
+    <main>
+        <div>SingleProduct</div>
+        <body className='mainProductContainer'>
+            <section className='leftSideContainer'>
+                <nav>Nav Bar PlaceHolder</nav>
+                <div className='immagesContainer'>
 
+                </div>
+            </section>
+            <section className='rightSideContainer'>
+                <header>
+                    The Name of the product
+                </header>
+                <div>reviews</div>
+                <div>description</div>
+                <span>two images</span>
+                <ul>select quantity divs</ul>
+                <div>Add to Cart button</div>
+            </section>
+        </body>
+        <section>
+            <div>
+                <h1>Reviews</h1>
+            </div>
+            <div>
+                Actual Reviews posted
+            </div>
+
+        </section>
+
+    </main>
 
   )
 }
