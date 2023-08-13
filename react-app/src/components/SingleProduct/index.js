51eb5269fc329532fc35f90f9696a8eb44c281a4
Zach Gierahn <zkg2789@yahoo.com>
2023-08-13 12:54:17 -0700

    update-single-product-wireframe

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import "./singleProduct.css"
 
 function SingleProduct() {
 
 
   return (
     <main>
-        <div>SingleProduct</div>
-        <body className='mainProductContainer'>
+        <div className='mainProductContainer'>
             <section className='leftSideContainer'>
-                <nav>Nav Bar PlaceHolder</nav>
-                <div className='immagesContainer'>
-
+                <nav className='trackWebLocation'>
+                    Nav Bar PlaceHolder
+                </nav>
+                <div className='imagesContainer'>
+                    <div>This is an image</div>
                 </div>
             </section>
             <section className='rightSideContainer'>
@@ -19,17 +21,41 @@ function SingleProduct() {
                 </header>
                 <div>reviews</div>
                 <div>description</div>
-                <span>two images</span>
-                <ul>select quantity divs</ul>
-                <div>Add to Cart button</div>
+                <span className='singleProductSpan'>
+                    <div>image 1</div>
+                    <div> image 2</div>
+                </span>
+                <ul className='selectPricesContainer'>
+                    <div>Select Price 1</div>
+                    <div>Select Price 2</div>
+                    <div>Select Price 3</div>
+
+                </ul>
+                <button>Add to Cart button</button>
             </section>
-        </body>
-        <section>
-            <div>
+        </div>
+        <section className='lowerProductSection'>
+            <div className='productReviewsHeader'>
                 <h1>Reviews</h1>
+                <div> Avg Review #</div>
+                <span>
+                    <div>Review Stars</div>
+                    <div># of Reviews</div>
+                </span>
             </div>
-            <div>
-                Actual Reviews posted
+            <div className='productReviewsContainter'>
+                <div className='singleReviewContainer'>
+                    <span className='reviewNameSpan'>
+                        <div>Customer name</div>
+                        <div>Verified Buyer</div>
+                    </span>
+                    <div>star rating</div>
+                    <div>Title</div>
+                    <div>description</div>
+                </div>
+                <div className='reviewDateContainer'>
+                    Date
+                </div>
             </div>
 
         </section>
